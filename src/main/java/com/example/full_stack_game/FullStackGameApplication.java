95a6b09ffcac4db020d1db2ecfb9e7d47de95e71
Robert <g00395612@atu.ie>
2023-11-25 01:51:36 +0000

    Initial commit

---
+++ src/main/java/com/example/full_stack_game/FullStackGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.full_stack_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/full_stack_game/FullStackGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.full_stack_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
