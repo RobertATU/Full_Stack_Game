e24a01968194a31f415e2d9e7cf639b9a155d921
Robert <g00395612@atu.ie>
2023-11-25 11:12:24 +0000

    Get mapping

--- src/main/java/com/example/full_stack_game/GameController.java
+++ src/main/java/com/example/full_stack_game/GameController.java
@@ -1,5 +1,6 @@
 package com.example.full_stack_game;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/game")
 public class GameController {
+    @Autowired
     private GameService gameService;
     @GetMapping
     public ResponseEntity<List<Game>> allGames(){
